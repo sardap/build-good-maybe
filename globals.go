b649c2345a9630d80a5415a290653755f964966b
sardap <sardap@users.noreply.github.com>
2022-10-09 08:38:32 +1100

    added options to set paths

--- globals.go
+++ globals.go
@@ -1,8 +1,22 @@
 package main
 
+import "os"
+
 var (
 	gritCmdPath  = "grit"
 	asepritePath = "aseprite"
 	bmp2gbaPath  = "bmp2gba.com"
 	gfxCache     *GraphicsCache
 )
+
+func init() {
+	if path := os.Getenv("GRIT_PATH"); path != "" {
+		gritCmdPath = path
+	}
+	if path := os.Getenv("ASEPRITE_PATH"); path != "" {
+		asepritePath = path
+	}
+	if path := os.Getenv("BMP2GBA_PATH"); path != "" {
+		bmp2gbaPath = path
+	}
+}
